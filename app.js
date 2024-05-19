ada81dd9b3508ba276add7b769e7dbc34b3cddbd
clark <cjeffreybda@outlook.com>
2024-05-18 20:19:17 -0700

    Created basic landing page with grid of static projects.

---
+++ app.js
@@ -0,0 +1,9 @@
+function showProject(i) {
+  // document.getElementById(i).style.visibility='visible';
+  // document.getElementById(i).style.opacity=1;
+  // alert('hi from ' + i + '!');
+}
+
+function closeProject() {
+  
+}
