4e69656aaf51e99bc144733606573a512334a505
Clark <cjeffreybda@outlook.com>
2024-05-19 17:08:12 -0700

    Added sorting to projects grid, tweaked CSS

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
+createProjectCards();
+
 function showProject(i) {
   // document.getElementById(i).style.visibility='visible';
   // document.getElementById(i).style.opacity=1;
   alert('hi from ' + i + '!');
-}
+};
+
+function createProjectCards() {
+  let projects = JSON.parse(localStorage.projects);
+  let projectsHTML = '';
+
+  projects.forEach((project) => {
+    let skills = project.skills;
+    let tag = project.tag;
+    let selected = localStorage.selected;
+
+    if (selected != 'sort-all' && ('sort-' + tag) != selected) {
+      return;
+    }
+
+    projectsHTML += `
+      <div class="project-card" onclick="showProject('${project.id}')">
+        <img class="project-image" src="./media/projects/${project.id}/${project.image}">
+        <div class="skills-flex">`;
+    
+      skills.forEach((skill) => {
+        projectsHTML += `
+        <p class="skill">${skill}</p>`;
+      })
+          
+      projectsHTML += `</div>
+        <div class="project-info">
+          <p class="project-title">${project.name}</p>
+          <p class="project-date">${project.date}</p>
+          <p class="project-blurb">${project.blurb}</p>
+        </div>
+      </div>`;
+  });
+
+  document.getElementById('project-grid').innerHTML = projectsHTML;
+};
+
+function changeSort(i) {
+  ['sort-all', 'sort-mech', 'sort-sftw'].forEach((id) => {
+    if (i == id) {
+      document.getElementById(id).className = 'button sort selected';
+      localStorage.selected = id;
+    }
+    else {
+      document.getElementById(id).className = 'button sort';
+    }
+  });
+  createProjectCards();
+};
 
 function closeProject() {
   
-}
+};
